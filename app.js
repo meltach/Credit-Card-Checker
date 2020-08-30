58e66008b43248b7ce9e6aeb1f05a919c02bc6d9
Melak-t <mcodes5723@gmail.com>
2020-08-30 00:13:56 -0400

    Little update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid4 = [7, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
 // Can be either valid or invalid
@@ -88,13 +88,15 @@ const idInvalidCardCompanies = invalidNums => {
     } else if(invalidNumsFirstIndex === 6){
       companyName.push('Discover');
     } else{
-      console.log('Company not found');
+      console.log('Company not found for Card Number:');
+      console.log(invalidNums[i]);
+      console.log('');
     }
   }
 
   const uniqueCompanyName = [...new Set(companyName)];
 
-  return uniqueCompanyName;
+  return 'Companies that issued faulty number: \n' + uniqueCompanyName;
 }
 
 console.log(idInvalidCardCompanies(inValidCards));
